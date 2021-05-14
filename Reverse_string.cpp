9d887e46cd8249ea37cda3135ea889b35f05eb6b
Rashmi Sharma <59957360+Rashmi1199@users.noreply.github.com>
2021-05-14 20:24:48 +0530

    Create Reverse_string.cpp

---
+++ Reverse_string.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void rev(string &s){
+    int n = s.length();
+    int fw;
+    for (fw = 0; fw < n / 2; fw++){
+        swap(s[fw], s[n - 1 - fw]);
+    }
+}
+
+int main(){
+    string s;
+    getline(cin, s);
+    rev(s);
+    cout << s;
+
+    return 0;
+}
