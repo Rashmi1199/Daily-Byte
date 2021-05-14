b74f871480aae75213a562a3fb075470bf27147a
Rashmi Sharma <59957360+Rashmi1199@users.noreply.github.com>
2021-05-14 21:17:28 +0530

    Update Reverse_string.cpp

--- Reverse_string.cpp
+++ Reverse_string.cpp
@@ -1,3 +1,15 @@
+/*
+Given a string, reverse all of its characters and return the resulting string.
+
+Ex: Given the following strings...
+
+“Cat”, return “taC”
+“The Daily Byte”, return "etyB yliaD ehT”
+“civic”, return “civic”
+
+*/
+
+
 #include<bits/stdc++.h>
 using namespace std;
 
